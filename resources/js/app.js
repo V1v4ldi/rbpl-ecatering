a2ad89130ffa95a13917f0db6470bd24f9c0cff4
Admin <rikzahasan0906@gmail.com>
2025-06-25 16:45:01 +0700

    fix vercel folder pushed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,9 @@
 import './bootstrap';
+import '../css/app.css';
+import '../images/a-login.png'
+import '../images/Alogin.jpeg';
+import '../images/homepage.png';
+import '../images/icon.png';
+import '../images/logo.png';
 import jQuery from 'jquery';
 window.$ = jQuery;
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 
+                    'resources/js/app.js',
+                    'resources/images/a-login.png',
+                    'resources/images/Alogin.jpeg',
+                    'resources/images/homepage.png',
+                    'resources/images/icon.png',
+                    'resources/images/logo.png',
+            ],
             refresh: true,
         }),
         tailwindcss(),
