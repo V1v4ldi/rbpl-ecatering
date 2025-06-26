06cd1a5810dadfa3e63a0c6c5fd25b18097f94cb
Admin <rikzahasan0906@gmail.com>
2025-06-26 12:25:53 +0700

    update api vite config

--- vite.config.js
+++ vite.config.js
@@ -30,4 +30,15 @@ export default defineConfig({
             '$': 'jQuery'
         },
     },
+    build: {
+        outDir: 'public/build', 
+        emptyOutDir: true, 
+        rollupOptions: {
+            output: {               
+                entryFileNames: `assets/[name]-[hash].js`,
+                chunkFileNames: `assets/[name]-[hash].js`,
+                assetFileNames: `assets/[name]-[hash].[ext]`
+            }
+        }
+    }
 });
